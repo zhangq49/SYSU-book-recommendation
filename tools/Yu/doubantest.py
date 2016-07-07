ee22186be5f8961ea28d1cda7c05acb04b1a3535
chenyuhang <496264889@qq.com>
2016-07-07 10:18:34 +0800

    2016/7/7 10:17

--- tools/Yu/doubantest.py
+++ tools/Yu/doubantest.py
@@ -1,12 +1,37 @@
 #-*- coding: UTF-8 -*-
 import urllib
+
+import urllib2
 import re
 from book import Book
 from bs4 import BeautifulSoup
 import bs4
 import json
+import random
+import cookielib
 
 bookUrlList = list()
+
+head_user_agent = ['Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko',
+                       'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.95 Safari/537.36',
+                       'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; rv:11.0) like Gecko)',
+                       'Mozilla/5.0 (Windows; U; Windows NT 5.2) Gecko/2008070208 Firefox/3.0.1',
+                       'Mozilla/5.0 (Windows; U; Windows NT 5.1) Gecko/20070309 Firefox/2.0.0.3',
+                       'Mozilla/5.0 (Windows; U; Windows NT 5.1) Gecko/20070803 Firefox/1.5.0.12',
+                       'Opera/9.27 (Windows NT 5.2; U; zh-cn)',
+                       'Mozilla/5.0 (Macintosh; PPC Mac OS X; U; en) Opera 8.0',
+                       'Opera/8.0 (Macintosh; PPC Mac OS X; U; en)',
+                       'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.12) Gecko/20080219 Firefox/2.0.0.12 Navigator/9.0.0.6',
+                       'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Win64; x64; Trident/4.0)',
+                       'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)',
+                       'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.2; .NET4.0C; .NET4.0E)',
+                       'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Maxthon/4.0.6.2000 Chrome/26.0.1410.43 Safari/537.1 ',
+                       'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.2; .NET4.0C; .NET4.0E; QQBrowser/7.3.9825.400)',
+                       'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:21.0) Gecko/20100101 Firefox/21.0 ',
+                       'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.92 Safari/537.1 LBBROWSER',
+                       'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0; BIDUBrowser 2.x)',
+                       'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.11 TaoBrowser/3.0 Safari/536.11']
+data = None
 
 #Get a page through its url
 def getHtml(url):
@@ -14,12 +39,22 @@ def getHtml(url):
     html = page.read()
     return html
 
+def getHtml2(url):
+    headers = {
+        'User-Agent':head_user_agent[random.randrange(0,len(head_user_agent))],
+        'Cookie':'viewed="25862578_26657570_26787931"; bid=6AKNIuCuCUM; gr_user_id=953140fa-8947-46bd-8f01-c6bbb186b931; __utma=30149280.1155223947.1467594905.1467594905.1467614162.2; __utmz=30149280.1467594905.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); ll="118281"'
+    }
+    req = urllib2.Request(url, data, headers)
+    response = urllib2.urlopen(req)
+    html = response.read()
+    return html
+
 def getTagList():
     tagList = []
-    html = getHtml("https://book.douban.com/tag/?view=type")
+    html = getHtml2("https://book.douban.com/tag/?view=type")
     soup = BeautifulSoup(html)
     for tag in soup.find_all("a", href = re.compile("^/tag/.*")):
-        tagList.append(tag.string)
+        tagList.append(tag.string.encode('UTF-8'))
     return tagList
 
 def printList(l):
@@ -58,7 +93,7 @@ def getTextList(content):
     return textList
 
 def getBookInf(book):
-    html = getHtml(book)
+    html = getHtml2(book)
     soup = BeautifulSoup(html)
     imgUrl = soup.find('img', src = re.compile("^https://"))['src']
 
@@ -108,19 +143,55 @@ def getBookInf(book):
                     sysuLibUrl='')
     printBook(bookItem)
 
-
+def getBookFromJson(bookJson):
+    print bookJson
+    uid = -1
+    print 'title: '
+    print bookJson['title'].encode('UTF-8')
+    name = bookJson['title'].encode('UTF-8')
+    print 'image: '
+    print bookJson['image'].encode('UTF-8')
+    imgUrl = bookJson['image'].encode('UTF-8')
+    print 'isbn: '
+    print bookJson['isbn13'].encode('UTF-8')
+    isbn = bookJson['isbn13'].encode('UTF-8')
+    print 'author: '
+    print bookJson['author']
+    author = bookJson['author']
+    print 'publisher'
+    print bookJson['publisher'].encode('UTF-8')
+    press = bookJson['publisher'].encode('UTF-8')
+    print 'rating: '
+    print bookJson['rating']['average']
+    doubanPoint = bookJson['rating']['average']
+    print 'rateSum: '
+    print bookJson['rating']['numRaters']
+    doubanRateSum = bookJson['rating']['numRaters']
+    print 'Summary: '
+    print bookJson['summary'].encode('UTF-8')
+    bookDescription = bookJson['summary'].encode('UTF-8')
+    print 'authorIntro: '
+    print bookJson['author_intro'].encode('UTF-8')
+    authorDescription = bookJson['author_intro'].encode('UTF-8')
+    sysuLibUrl = ''
+    return Book(uid, name, imgUrl,
+      isbn, author, press, doubanPoint, doubanRateSum,
+      bookDescription, authorDescription, sysuLibUrl)
 
 def getBookJsonFromId(Id):
-    return json.loads(getHtml('https://api.douban.com/v2/book/' + str(Id)))
+    bookStr = (getHtml2('https://api.douban.com/v2/book/' + str(Id)))
+    return json.loads(bookStr)
 
 def getIdFromUrl(url):
     sub = url.split('/')
-    return sub[-2]
+    for x in sub[::-1]:
+        if not x == '':
+            return x
 
 def getTagInf(tag):
     index = 0
     while(1):
-        html = getHtml("https://book.douban.com/tag/" + tag.encode('UTF-8') + "?start=" + str(index*20))
+        html = getHtml2("https://book.douban.com/tag/" + tag + "?start=" + str(index*20))
         index = index + 1
         print 'index:' + str(index) + '###########'
         soup = BeautifulSoup(html)
@@ -144,14 +215,18 @@ def searchTagList():
 def test():
     searchTagList()
 
-testBook = getHtml("https://api.douban.com/v2/book/25862578")
-print testBook
-bookJson = json.loads(testBook)
-print bookJson['rating']
 
-test()
-
+#bookId = getIdFromUrl("https://api.douban.com/v2/book/25862578")
+#bookJson = getBookJsonFromId(bookId)
+#book = getBookFromJson(bookJson)
+#printBook(book)
 
 
 
+getTagInf('小说')
+for bookUrl in bookUrlList:
+    bookId = getIdFromUrl(bookUrl)
+    print bookUrl
+    bookJson = getBookJsonFromId(bookId)
+    getBookFromJson(bookJson)
 
